d5e67f094533a37234f8e8d4528ce857d2bbdbf6
dev-bearabbit <cjstk3940@naver.com>
2024-11-20 00:49:13 +0900

    update

--- src/lottie.rs
+++ src/lottie.rs
@@ -8,7 +8,7 @@ export function loadLottieAnimation() {
         renderer: 'svg',
         loop: true,
         autoplay: true,
-        path: '/lottie/snow-effect.json',
+        path: '/tree-drawing-challenge/lottie/snow-effect.json',
         rendererSettings: {
         preserveAspectRatio: 'xMidYMid slice' // 전체 화면에 맞도록 크기 조정
         }
